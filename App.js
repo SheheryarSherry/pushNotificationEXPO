406454bd0fc71db1c1f8c151209460000ea28557
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2022-05-27 01:18:07 +0500

    hello

--- App.js
+++ App.js
@@ -1,10 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Button, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+    };
+  },
+});
 
 export default function App() {
+  const [pushToken, setPushToken] = useState();
+  useEffect(() => {
+    Notifications.getPermissionsAsync()
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          return Notifications.requestPermissionsAsync();
+        }
+        return statusObj;
+      })
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          // alert();
+          throw new Error("Permission not granted.");
+        }
+      })
+      .then(() => {
+        console.log("Getting token..");
+        return Notifications.getExpoPushTokenAsync();
+      })
+      .then((response) => {
+        const token = response.data;
+        console.log('Token', token)
+        setPushToken(token);
+      })
+      .catch((err) => {
+        console.log(err);
+        return null;
+      });
+  }, []);
+
+  useEffect(() => {
+    const backgroundSubscription =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    const foregroundSubscription =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+    return () => {
+      backgroundSubscription.remove();
+      foregroundSubscription.remove();
+    };
+  }, []);
+
+  const triggerNotificationHandler = () => {
+
+    fetch("https://exp.host/--/api/v2/push/send/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-Encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: pushToken,
+        data: { extraData: "this is test notification " },
+        title: "CHIP",
+        body: "Bht bhook lagi hai? tu ja k khao na notification q parh ry ho ajeeb!!!",
+      }),
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Send Push Notification"
+        onPress={triggerNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +90,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
